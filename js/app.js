4eea0f51384251bcd95454f6676eebf601961dc8
Red-Flags <88331010+Red-Flags@users.noreply.github.com>
2021-08-02 19:32:37 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,593 @@
+// Collection Of Cards
+// Made by Sparsh
+const cards = [{
+    card: `Believes you are the most attractive person they've ever seen`,
+}, {
+    card: `Fiercely loyal`,
+}, {
+    card: `Trusting`,
+}, {
+    card: `Respectful`,
+}, {
+    card: `Honest`,
+}, {
+    card: `Patient`,
+}, {
+    card: `Reliable`,
+}, {
+    card: `Courteous`,
+}, {
+    card: `Funniest person in the world`,
+}, {
+    card: `Affectionate`,
+}, {
+    card: `Good listener`,
+}, {
+    card: `Supportive`,
+}, {
+    card: `Generous`,
+}, {
+    card: `Driven`,
+}, {
+    card: `Perfect communicator`,
+}, {
+    card: `Always makes time for you`,
+}, {
+    card: `Always gives you perfect gifts`,
+}, {
+    card: `Perfect amount of physical affection`,
+}, {
+    card: `Adventurous`,
+}, {
+    card: `Loves you unconditionally`,
+}, {
+    card: `In touch with their feelings`,
+}, {
+    card: `Your parents love them`,
+}, {
+    card: `Your friends and enemies are jealous`,
+}, {
+    card: `Owns a magic carpet`,
+}, {
+    card: `Volunteers with elderly people`,
+}, {
+    card: `Loves dogs`,
+}, {
+    card: `Loves cats`,
+}, {
+    card: `Loves theater`,
+}, {
+    card: `Animal shelter volunteer`,
+}, {
+    card: `Travels a lot`,
+}, {
+    card: `Really into wine`,
+}, {
+    card: `Loves video games`,
+}, {
+    card: `Has front row tickets to every concert`,
+}, {
+    card: `Has front row tickets to every sporting event`,
+}, {
+    card: `Loves sports`,
+}, {
+    card: `Loves the outdoors`,
+}, {
+    card: `Loves staying in`,
+}, {
+    card: `Owns your favorite museum`,
+}, {
+    card: `Same favorite movies`,
+}, {
+    card: `Same favorite books`,
+}, {
+    card: `Same favorite foods`,
+}, {
+    card: `Same favorite music`,
+}, {
+    card: `Lives off the grid`,
+}, {
+    card: `Loves children`,
+}, {
+    card: `Millionaire`,
+}, {
+    card: `Most physically attractive person you have ever seen`,
+}, {
+    card: `Owns a vineyard`,
+}, {
+    card: `Owns an adorable coffee shop`,
+}, {
+    card: `Lives on 100 beachfront acres`,
+}, {
+    card: `Owns a solid gold private jet`,
+}, {
+    card: `Has a magic bag that contains unlimited cheese`,
+}, {
+    card: `Owns a 500-foot yacht`,
+}, {
+    card: `Owns an island in the Bahamas`,
+}, {
+    card: `Owns one of every car`,
+}, {
+    card: `Owns 100 horses`,
+}, {
+    card: `Owns your favorite sports team`,
+}, {
+    card: `Owns a penthouse suite in your favorite city`,
+}, {
+    card: `Gives billions to your favorite charity`,
+}, {
+    card: `Owns a time machine`,
+}, {
+    card: `Lives in a castle`,
+}, {
+    card: `Has the best hair ever`,
+}, {
+    card: `Owns a unicorn`,
+}, {
+    card: `Genius-level IQ`,
+}, {
+    card: `Owns a successful startup`,
+}, {
+    card: `Best sex ever`,
+}, {
+    card: `Can get you both into literally anywhere`,
+}, {
+    card: `You'll never have to work while you date them`,
+}, {
+    card: `You'll never get sick while you date them`,
+}, {
+    card: `You literally will not age while you date them`,
+}, {
+    card: `Looks exactly like _____`,
+}, {
+    card: `Always smells like _____`,
+}, {
+    card: `Has a mansion in _____`,
+}, {
+    card: `Speaks every romantic language`,
+}, {
+    card: `Drives a _____`,
+}, {
+    card: `Is a professional _____`,
+}, {
+    card: `Owns a _____`,
+}, {
+    card: `Knows everything about _____`,
+}, {
+    card: `Volunteers for _____`,
+}, {
+    card: `You will never gain weight while you date them`,
+}, {
+    card: `Has the voice of an angel`,
+}, {
+    card: `Owns a goose that lays designer clothing`,
+}, {
+    card: `Has a cellar full of _____`,
+}, {
+    card: `Professional surfer`,
+}, {
+    card: `Rocket scientist`,
+}, {
+    card: `Model`,
+}, {
+    card: `Professional football player`,
+}, {
+    card: `Famous artist`,
+}, {
+    card: `College English professor`,
+}, {
+    card: `Firefighter`,
+}, {
+    card: `Astronaut`,
+}, {
+    card: `President`,
+}, {
+    card: `Rodeo champion`,
+}, {
+    card: `Professional wrestler`,
+}, {
+    card: `Famous movie star`,
+}, {
+    card: `Famous musician`,
+}, {
+    card: `Famous rapper`,
+}, {
+    card: `Porn star`,
+}, {
+    card: `Supermodel`,
+}, {
+    card: `Professional chef`,
+}, {
+    card: `Is never gonna give you up / Let you down / Run around / Or desert you`,
+}, {
+    card: `World's best hair stylist`,
+}, {
+    card: `Brain surgeon`,
+}, {
+    card: `Lawyer`,
+}, {
+    card: `CEO`,
+}, {
+    card: `World's best advertising executive`,
+}, {
+    card: `Airline pilot`,
+}, {
+    card: `Marine`,
+}, {
+    card: `Spin instructor`,
+}, {
+    card: `Bestselling author`,
+}, {
+    card: `YouTube celebrity`,
+}, {
+    card: `Lumberjack`,
+}, {
+    card: `Organic farmer`,
+}, {
+    card: `Stunt double`,
+}, {
+    card: `Park ranger`,
+}, {
+    card: `Ski instructor`,
+}, {
+    card: `Photographer`,
+}, {
+    card: `Famous fashion designer`,
+}, {
+    card: `Is royalty`,
+}, {
+    card: `Is a genie`,
+}, {
+    card: `World champion _____`,
+}, {
+    card: `World champion hunter`,
+}, {
+    card: `Won the medal of honor`,
+}, {
+    card: `Was on the news for rescuing a kitten`,
+}, {
+    card: `Best-smelling person in the world`,
+}, {
+    card: `Yoga instructor`,
+}, {
+    card: `The hot barista`,
+}, {
+    card: `Has an 8-pack`,
+}, {
+    card: `Olympic gymnast`,
+}, {
+    card: `Pick an actor/actress`,
+}, {
+    card: `Never arrives prematurely`,
+}, {
+    card: `Loves to cuddle`,
+}, {
+    card: `Is a vampire... / the sexy kind`,
+}, {
+    card: `Has the most attractive _____ you've ever seen`,
+}, {
+    card: `The lead singer of your favorite band`,
+}, {
+    card: `Beautiful eyes`,
+}, {
+    card: `Completely blind to all of your flaws`,
+}, {
+    card: `Spontaneous and fun`,
+}, {
+    card: `Makes you feel secure`,
+}, {
+    card: `Has the hottest friends`,
+}, {
+    card: `Has a twin`,
+}, {
+    card: `Life is the true story that _____ is based on`,
+}, {
+    card: `Can fly`,
+}, {
+    card: `Superhero`,
+}, {
+    card: `Loves to laugh`,
+}, {
+    card: `Makes scented candles`,
+}, {
+    card: `Loves the '80s`,
+}, {
+    card: `Cooks all your favorite foods`,
+}, {
+    card: `Knows all the words to your favorite children's TV theme songs`,
+}, {
+    card: `Never blames you for anything`,
+}, {
+    card: `Can get you free drinks forever at any bar in the world`,
+}, {
+    card: `Creates shelters for the homeless`,
+}, {
+    card: `Their kisses cure cancer`,
+}, {
+    card: `Is a great conversationalist`,
+}, {
+    card: `Gives the best hugs`,
+}, {
+    card: `Gives you multiple orgasms`,
+}, {
+    card: `Is a merperson`,
+}, {
+    card: `Always knows how to stop you from crying`,
+}, {
+    card: `Can multiply themselves`,
+}, {
+    card: `An amazing dancer`,
+}, {
+    card: `Always defends you`,
+}, {
+    card: `Can get you a table at any 5-star restaurant any time any place`,
+}, {
+    card: `Can fire your most hated co-worker or boss`,
+}, {
+    card: `Their farts smell like flowers`,
+}, {
+    card: `Never farts`,
+}, {
+    card: `Their morning breath smells like fresh-baked cookies`,
+}, {
+    card: `Owns Mars`,
+}, {
+    card: `Is a night person and a morning person`,
+}, {
+    card: `Loves their job`,
+}, {
+    card: `Has all the same hobbies as you`,
+}, {
+    card: `Can talk to animals`,
+}, {
+    card: `Owns real dinosaurs`,
+}, {
+    card: `Gives great massages`,
+}, ]
+
+const fwags = [
+    {fwag: `Thinks they're a Jedi`,},
+    {fwag: `Civil War reenactor`,},
+    {fwag: `Super super racist`,},
+    {fwag: `Sexist`,},
+    {fwag: `Really stupid`,},
+    {fwag: `Owes the government one million dollars`,},
+    {fwag: `Death row inmate`,},
+    {fwag: `Zero percent sexually attractive to you`,},
+    {fwag: `Thinks you're ugly`,},
+    {fwag: `Their pet name for you is "trash bag"`,},
+    {fwag: `Is always covered in ants`,},
+    {fwag: `Really really hates children`,},
+    {fwag: `Only speaks in horrible puns`,},
+    {fwag: `Is a merperson`,},
+    {fwag: `Sees the ghost of everyone who has ever died`,},
+    {fwag: `Is literally always watching porn`,},
+    {fwag: `Is addicted to crack`,},
+    {fwag: `Is addicted to prescription drugs`,},
+    {fwag: `Is a train wreck`,},
+    {fwag: `Backseat driver`,},
+    {fwag: `Reads over your shoulder (out loud)`,},
+    {fwag: `Is six years old`,},
+    {fwag: `Only speaks in sarcasm`,},
+    {fwag: `Farts every time they kiss you`,},
+    {fwag: `Kicks every animal they see`,},
+    {fwag: `Worst-smelling person in the world`,},
+    {fwag: `Amateur cannibal`,},
+    {fwag: `Dabbles in torture`,},
+    {fwag: `Uses the word "chillax" in every sentence`,},
+    {fwag: `Body type: Dumpster`,},
+    {fwag: `Has a permanent fedora`,},
+    {fwag: `Basic`,},
+    {fwag: `Spends every date using dating apps`,},
+    {fwag: `Literally can't make decisions`,},
+    {fwag: `Has to pee every five minutes for five minutes`,},
+    {fwag: `Sold your pets on Craigslist`,},
+    {fwag: `Only speaks in yelling`,},
+    {fwag: `Doesn't know the difference between your and you're`,},
+    {fwag: `Only speaks in business buzzwords`,},
+    {fwag: `Eats crayons`,},
+    {fwag: `Has a dozen kids from a dozen partners`,},
+    {fwag: `Is wanted in every state except Florida / (Because Florida doesn't want them)`,},
+    {fwag: `Is a hobbit`,},
+    {fwag: `Has nineteen kids and counting`,},
+    {fwag: `Is building an ark`,},
+    {fwag: `Snorts _____ / Really wants you to try it`,},
+    {fwag: `Will only eat if you play airplane with their food`,},
+    {fwag: `Literally always on a Segway`,},
+    {fwag: `Cries themself to sleep every night`,},
+    {fwag: `Takes you to _____ on the first date`,},
+    {fwag: `Is Nickelback`,},
+    {fwag: `Counts your calories`,},
+    {fwag: `Can't keep a job longer than twenty minutes`,},
+    {fwag: `Hair is transplanted from cadaver butts`,},
+    {fwag: `Looks and smells just like your dad`,},
+    {fwag: `Throws up every time they see your face`,},
+    {fwag: `Is your clone`,},
+    {fwag: `Melts if they get wet`,},
+    {fwag: `Yawns every time you start a sentence`,},
+    {fwag: `First name is "Poop"`,},
+    {fwag: `College nickname: Puke Crotch`,},
+    {fwag: `Still in fourth grade`,},
+    {fwag: `Teleports to the North Pole every time they say the word "love"`,},
+    {fwag: `Every time you think about them you dry heave`,},
+    {fwag: `Takes you to couples therapy on the first date`,},
+    {fwag: `Only speaks in boy band lyrics`,},
+    {fwag: `Hits on every server`,},
+    {fwag: `Has never spelled a word correctly`,},
+    {fwag: `Collects human skulls / Says they're "practically free!"`,},
+    {fwag: `Steals something from every store they enter`,},
+    {fwag: `Compulsive liar`,},
+    {fwag: `Is eligible for the Make-A-Wish Foundation`,},
+    {fwag: `Calls you by exe's name / Every time`,},
+    {fwag: `Always wearing a Bluetooth headset`,},
+    {fwag: `Is the person you hated the most in high school / and literally hasn't changed`,},
+    {fwag: `Posts all of your texts and emails online`,},
+    {fwag: `3-foot fingernails`,},
+    {fwag: `Keeps asking if your best friend is "down"`,},
+    {fwag: `Only communicates with you via fax`,},
+    {fwag: `Only speaks Klingon`,},
+    {fwag: `Literally won't stop talking about their mother`,},
+    {fwag: `Sleep-puncher`,},
+    {fwag: `Has scissor hands`,},
+    {fwag: `Describes all food as "yummy yummers"`,},
+    {fwag: `Writes erotic Lassie fan fiction`,},
+    {fwag: `Won't allow wi-fi in the house`,},
+    {fwag: `Refuses to wash hands`,},
+    {fwag: `Celibate`,},
+    {fwag: `Has plexiglass cheeks so that you can see inside their mouth`,},
+    {fwag: `Responds to everything to say with "that's what she said"`,},
+    {fwag: `Isn't potty trained`,},
+    {fwag: `Is a serial killer`,},
+    {fwag: `Only speaks in Disney song lyrics`,},
+    {fwag: `Still uses a flip phone`,},
+    {fwag: `Has two other spouses`,},
+    {fwag: `Only eats deep-fried food`,},
+    {fwag: `Lives off the grid`,},
+    {fwag: `Tries to pay for everything with farm animals`,},
+    {fwag: `Never stops playing the ukulele`,},
+    {fwag: `Regularly hosts tea parties for dolls`,},
+    {fwag: `Is the oldest living person`,},
+    {fwag: `Constantly taking selfies`,},
+    {fwag: `Has _____ for hands`,},
+    {fwag: `Says "Can you not" while you cry at a funeral`,},
+    {fwag: `Adds you as an emergency contact on the first date`,},
+    {fwag: `Literally only talks about themselves`,},
+    {fwag: `Only responds to texts with "k"`,},
+    {fwag: `Is related to you`,},
+    {fwag: `Incapable of all emotion`,},
+    {fwag: `Is actually two children stacked on top of each other in a trench coat`,},
+    {fwag: `Name drops during dirty talk`,},
+    {fwag: `Tells you to calm down after everything you say`,},
+    {fwag: `You are allergic to them`,},
+    {fwag: `Greets people by sniffing their butts`,},
+    {fwag: `Is totally married`,},
+    {fwag: `Constantly trying to bang your friends`,},
+    {fwag: `Refers to their genitals as their "no-no"`,},
+    {fwag: `Rigged to a time bomb`,},
+    {fwag: `Is a zombie`,},
+    {fwag: `Conveniently forgets wallet every date night / Always has ID though`,},
+    {fwag: `Calls you a "4" to your face`,},
+    {fwag: `Steals locks of your hair`,},
+    {fwag: `Their kisses taste like poop / and they love kissing`,},
+    {fwag: `Repeats your jokes but louder`,},
+    {fwag: `Compulsively licks doorknobs`,},
+    {fwag: `Physically incapable of asking a question`,},
+    {fwag: `Jealous of your pet`,},
+    {fwag: `Afraid of sunlight`,},
+    {fwag: `Speaks with a constantly changing foreign accent`,},
+    {fwag: `Literally always drunk`,},
+    {fwag: `Brings ex to first date`,},
+    {fwag: `Brings mom to first date`,},
+    {fwag: `Burps and blows in your face`,},
+    {fwag: `In a band called "Genocide Warts"`,},
+    {fwag: `Literally never stops talking`,},
+    {fwag: `Constantly plotting to kill you`,},
+    {fwag: `Open mouth kisses their pets`,},
+    {fwag: `Immediately tries to kill you with a fork`,},
+    {fwag: `Super clingy`,},
+    {fwag: `Open mouth kisses their siblings`,},
+    {fwag: `Literally won't talk to you if you're part of the wrong Hogwarts house`,},
+    {fwag: `Greets you with a headbutt`,},
+    {fwag: `Has slept with literally everyone`,},
+    {fwag: `Always picking their nose`,},
+    {fwag: `Only speaks in hashtags`,},
+    {fwag: `Demands to pop all of your zits for you`,},
+    {fwag: `Recites all the lines from "Titanic" in their sleep`,},
+    {fwag: `Narrates everything they do out loud`,},
+    {fwag: `Puts "air quotes" around "every word" they say`,},
+    {fwag: `Knocks things out of strangers' hands while walking down the street`,},
+    {fwag: `Charges you a quarter every time you say a curse word`,},
+    {fwag: `Is dead`,},
+    {fwag: `Is literally always naked`,},
+    {fwag: `Talks dirty to all of their food`,},
+    {fwag: `Has the names of all of their exes tattooed on their face`,},
+    {fwag: `Only wears clothing made of human hair`,},
+    {fwag: `Wears diapers because it's "more convenient"`,},
+    {fwag: `Can only speak in lines from _____`,},
+    {fwag: `Only laughs at their own jokes`,},
+    {fwag: `Keeps changing the subject to talk about how not racist they are`,},
+    {fwag: `Just got out of prison / but won't tell you what they were in for`,},
+    {fwag: `50 Shades of Grey is literally the only book they've read`,},
+    {fwag: `Doesn't think women should vote`,},
+    {fwag: `Has been stalking you since middle school`,},
+    {fwag: `Is 40 years older than they were in their profile picture`,},
+    {fwag: `Is a vampire... / Not the sexy kind`,},
+    {fwag: `Winks at you after everything they say ;)`,},
+    {fwag: `Swears you look exactly like their sister / But says "that's a good thing"`,},
+    {fwag: `Tinders during sex`,},
+    {fwag: `Wears a backpack full of _____ during sex`,},
+    {fwag: `Men's rights activist`,},
+    {fwag: `Is a 60 year old virgin`,},
+    {fwag: `Ends every conversation with "you're dismissed"`,},
+    {fwag: `Sells home-made pubic hair wigs`,},
+    {fwag: `Still really into Myspace`,},
+    {fwag: `Their sex tape with _____ just went viral`,},
+    {fwag: `Can only have conversations through puppets`,},
+    {fwag: `Always wears a luchador mask`,},
+    {fwag: `Has half a beard / Absolutely refuses to shave`,},
+    {fwag: `Swears they're eighteen`,},
+    {fwag: `Yells "ear muffs!" whenever they hear a bad word`,},
+    {fwag: `Amateur vigilante`,},
+    {fwag: `Won't stop juggling`,},
+    {fwag: `Resting dumb face`,},
+    {fwag: `Keeps talking about their "hot cousin"`,},
+    {fwag: `Talks exclusively in emojis`,},
+    {fwag: `Refuses to delete online dating profile`,},
+    {fwag: `Always bragging about past sexual conquests`,},
+    {fwag: `Can only eat out of someone else's hand`,},
+    {fwag: `Uses way way way too much tongue`,},
+    {fwag: `Refuses to cover face while sneezing`,},
+    {fwag: `Refuses to look up from phone`,},
+    {fwag: `Keeps talking about someone who looks just like you / Only better!`,},
+    {fwag: `Thinks Twilight deserved an Oscar`,},
+    {fwag: `Is afraid of stoplights`,},
+    {fwag: `Doesn't understand your jokes`,},
+    {fwag: `Doesn't believe in oral sex`,},
+    {fwag: `Laughs incessantly during sex`,},
+    {fwag: `Is a revenge porn mogul`,},
+    {fwag: `Live-tweets your entire date`,},
+    {fwag: `Slept with one of your parents / Recently`,},
+    {fwag: `For as long as you date them all your friends will hate you`,},
+    {fwag: `Always dresses like a clown`,},
+    {fwag: `Constantly freestyles / Badly`,},
+    {fwag: `Has a _____ fetish`,},
+    {fwag: `Only speaks in motivational quotes`,},
+    {fwag: `The two halves of their face don't match / Both are attractive but look totally different`,},
+    {fwag: `Doesn't know or understand any word that starts with the letter "D"`,},
+    {fwag: `Only has eyes for you / They literally can't see anyone in the world except you`,},
+    {fwag: `Has a superpower / It's seeing through people's clothes`,},
+    {fwag: `Always takes the joke three steps too far`,},
+    {fwag: `Is on every fad diet at the same time`,},
+    {fwag: `Calls their pets their children`,},
+    {fwag: `Static shocks you every time you touch`,},
+    {fwag: `Is allergic to all of your favorite foods`,},
+    {fwag: `Says "like", like all the time`,},
+    {fwag: `Literally has the face of a pug`,},
+    {fwag: `Begins every sentence with "actually"`,},
+    {fwag: `Lives in a bedroom with eight dudes / Including one named "Jug"`,},
+    {fwag: `Doesn't like music... / any of it`,},
+    {fwag: `Thinks they're smarter than you`,},
+    {fwag: `As long as you date them you can only eat food off the dollar menu`,}, 
+]
+// New Cards Button
+const cardBtn = document.getElementById('card-btn');
+// Get cards and flags
+const card = document.querySelectorAll('.card');
+const fwag = document.querySelectorAll('.fwag');
+// Event Handler
+cardBtn.addEventListener('click', () => {
+
+    // card.innerHTML = cards[random].card;
+    // fwag.innerHTML = cards[random].fwag;
+
+    for(let x of card){
+        let random = Math.floor(Math.random() * cards.length);
+        x.innerHTML = cards[random].card;
+    }
+    for(let x of fwag){
+        let random = Math.floor(Math.random() * fwags.length);
+        x.innerHTML = fwags[random].fwag;
+    }
+    console.log("Clicked!!");
+})
